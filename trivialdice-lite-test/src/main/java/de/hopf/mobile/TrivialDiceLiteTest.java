07a812b59440637acf859cbee387b96a4c27cc5e
Hefezopf <info@hopf-it.de>
2014-07-17 22:25:30 +0200

    fix

--- trivialdice-lite-test/src/main/java/de/hopf/mobile/TrivialDiceLiteTest.java
+++ trivialdice-lite-test/src/main/java/de/hopf/mobile/TrivialDiceLiteTest.java
@@ -5,6 +5,6 @@ package de.hopf.mobile;
 public class TrivialDiceLiteTest extends TrivialDiceBaseTest {
     public TrivialDiceLiteTest() {
         super(StartDice.class, R.id.start, R.id.link_text,
-                "http://market.android.com/details?id=de.hopf.mobile.trivialdiceXXXX");
+                "http://market.android.com/details?id=de.hopf.mobile.trivialdice");
     }
 }
