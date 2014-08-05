fcd44e4616fdc891753dba3d5820636b7b6a956d
Hefezopf <info@hopf-it.de>
2014-08-05 19:50:33 +0200

    test added

---
+++ trivialbottlespin-lite-test/src/main/java/test/de/hopf/android/bottlespin/lite/TrivialBottleSpinLiteTest.java
@@ -0,0 +1,12 @@
+package test.de.hopf.android.bottlespin.lite;
+
+import de.hopf.android.bottlespin.lite.R;
+import de.hopf.android.bottlespin.lite.StartDice;
+import de.hopf.mobile.TrivialDiceBaseTest;
+
+public class TrivialBottleSpinLiteTest extends TrivialDiceBaseTest
+{ 
+    public TrivialBottleSpinLiteTest() {
+        super(StartDice.class, R.id.start, R.id.link_text);
+    }   
+}
---
+++ trivialbottlespin-premium-test/src/main/java/test/de/hopf/android/bottlespin/TrivialBottleSpinTest.java
@@ -0,0 +1,11 @@
+package test.de.hopf.android.bottlespin;
+
+import de.hopf.android.bottlespin.StartDice;
+import de.hopf.mobile.TrivialDicePremiumBaseTest;
+
+public class TrivialBottleSpinTest extends TrivialDicePremiumBaseTest
+{ 
+    public TrivialBottleSpinTest() {
+        super(StartDice.class);
+    }       
+}
