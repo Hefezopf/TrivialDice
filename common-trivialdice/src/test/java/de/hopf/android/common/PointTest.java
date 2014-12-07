a5fc0170d03c1a39d173203b6e46298420108e4e
Hefezopf <info@hopf-it.de>
2014-12-07 09:21:08 +0100

    TEsts

--- common-trivialdice/src/test/java/de/hopf/android/common/PointTest.java
+++ common-trivialdice/src/test/java/de/hopf/android/common/PointTest.java
@@ -3,15 +3,21 @@ package de.hopf.android.common;
 import org.junit.Assert;
 import org.junit.Test;
 
-import de.hopf.android.common.Point;
-
 public class PointTest
 {
-	float x, y;
+    float x, y;
 
     @Test
-	public void testToString() {
+    public void testToString() {
         Point p = new Point(1 ,2);
-		Assert.assertEquals(p.toString(), "1, 2");
-	}
+        Assert.assertEquals(p.toString(), "1, 2");
+    }
+
+    @Test
+    public void testGetter() {
+        Point p = new Point(1 ,2);
+        Assert.assertEquals(p.getX(), 1);
+        Assert.assertEquals(p.getY(), 2);
+    }
+
 }
