806221e91533f4ffffa94b9cff1c1f601e1d80b7
Markus Hopf <markus@hopf-it.de>
2014-12-08 16:35:43 +0100

    Create LetterDiceDrawableTest.java

---
+++ common-trivialdice/src/test/java/de/hopf/android/common/drawable/custom/LetterDiceDrawableTest.java
@@ -0,0 +1,20 @@
+package de.hopf.android.common.drawable.custom;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import de.hopf.android.common.ItemAmountType;
+
+public class LetterDiceDrawableTest extends BaseDiceDrawableTest {
+
+	@Before
+	public void setup() {
+		drawable = new LetterDiceDrawable(1, 2, 3);
+		drawable.initDrawableList(ItemAmountType.SIX);
+	}
+
+	@Test
+	public void testSetupDice() {
+		((LetterDiceDrawable) drawable).setupDice(1, 2, 3, 4, 5, initList());
+	}
+}
