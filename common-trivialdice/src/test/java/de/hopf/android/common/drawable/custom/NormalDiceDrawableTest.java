ebb85085671db24d09c3d3d0c575c47fbbb2caf6
Markus Hopf <markus@hopf-it.de>
2014-12-08 16:36:29 +0100

    Update NormalDiceDrawableTest.java

--- common-trivialdice/src/test/java/de/hopf/android/common/drawable/custom/NormalDiceDrawableTest.java
+++ common-trivialdice/src/test/java/de/hopf/android/common/drawable/custom/NormalDiceDrawableTest.java
@@ -1,34 +1,20 @@
 package de.hopf.android.common.drawable.custom;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import de.hopf.android.common.ItemAmountType;
-import de.hopf.android.common.Point;
 
-public class NormalDiceDrawableTest {
+public class NormalDiceDrawableTest extends BaseDiceDrawableTest {
 
-    private NormalDiceDrawable drawable;
+	@Before
+	public void setup() {
+		drawable = new NormalDiceDrawable(1, 2, 3);
+		drawable.initDrawableList(ItemAmountType.SIX);
+	}
 
-    @Before
-    public void setup(){
-        drawable = new NormalDiceDrawable(1, 2, 3);
-        drawable.initDrawableList(ItemAmountType.SIX);
-    }
-
-    @Ignore
-    @Test
-    public void testSetupDice() {
-        List<List<Point>> list = new ArrayList<List<Point>>();
-        List<Point> listInner = new ArrayList<Point>();
-        listInner.add(new Point(1,1));
-        listInner.add(new Point(2,2));
-        list.add(listInner);
-        drawable.setupDice(1 ,2, 3, 4, 5, list);
-    }
-
- }
+	@Test
+	public void testSetupDice() {
+		((NormalDiceDrawable) drawable).setupDice(1, 2, 3, 4, 5, initList());
+	}
+}
