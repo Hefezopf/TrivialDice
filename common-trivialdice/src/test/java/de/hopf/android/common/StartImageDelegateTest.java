8071b115a4dcd339f174c887e1f4835a1ee5939e
Markus Hopf <markus@hopf-it.de>
2014-12-08 16:32:43 +0100

    Update StartImageDelegateTest.java

--- common-trivialdice/src/test/java/de/hopf/android/common/StartImageDelegateTest.java
+++ common-trivialdice/src/test/java/de/hopf/android/common/StartImageDelegateTest.java
@@ -1,21 +1,33 @@
 package de.hopf.android.common;
 
 import org.junit.Assert;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.robolectric.RobolectricTestRunner;
 
 import android.app.Activity;
-import de.hopf.android.common.drawable.Drawable;
+import android.content.res.Resources;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
+import de.hopf.android.common.drawable.base.BaseDrawable;
 
-@Ignore
+@RunWith(RobolectricTestRunner.class)
 public class StartImageDelegateTest {
 
     @Test
     public void testPremium() {
         Activity activity = Mockito.mock(Activity.class);
-        Drawable drawable = Mockito.mock(Drawable.class);
-        DrawViewImage drawViewImage = new DrawViewImage(activity, activity.getWindowManager(), 1, drawable, false);
+        ContextData context = Mockito.mock(ContextData.class);
+        Resources resources = Mockito.mock(Resources.class);
+        Mockito.when(resources.getDisplayMetrics()).thenReturn(Mockito.mock(DisplayMetrics.class));
+        Mockito.when(context.getResources()).thenReturn(resources);
+        WindowManager windowManager = Mockito.mock(WindowManager.class);
+        Mockito.when(windowManager.getDefaultDisplay()).thenReturn(Mockito.mock(Display.class));
+        BaseDrawable drawable = Mockito.mock(BaseDrawable.class);
+        
+        DrawViewImage drawViewImage = new DrawViewImage(context, windowManager, 1, drawable, false);
         StartImageDelegate d = new StartImageDelegate(activity, 1, 2, drawable, drawViewImage);
         Assert.assertNotNull(d);
         d.setNumber(1);
