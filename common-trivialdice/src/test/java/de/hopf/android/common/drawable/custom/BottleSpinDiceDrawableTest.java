d0d155d4a8752a31149f081b6c10f3bd9e1af170
Markus Hopf <markus@hopf-it.de>
2014-12-08 16:30:39 +0100

    Create BottleSpinDiceDrawableTest.java

---
+++ common-trivialdice/src/test/java/de/hopf/android/common/drawable/custom/BottleSpinDiceDrawableTest.java
@@ -0,0 +1,37 @@
+package de.hopf.android.common.drawable.custom;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.robolectric.RobolectricTestRunner;
+
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.util.DisplayMetrics;
+import de.hopf.android.common.ContextData;
+import de.hopf.android.common.DrawViewImage;
+import de.hopf.android.common.ItemAmountType;
+
+@RunWith(RobolectricTestRunner.class)
+public class BottleSpinDiceDrawableTest extends BaseDiceDrawableTest {
+
+	@Before
+	public void setup() {
+		drawable = new BottleSpinDrawable(Mockito.mock(Resources.class), 1, 2, 3, 4);
+		drawable.initDrawableList(ItemAmountType.SIX);
+	}
+
+	@Test
+	public void testDrawShape() {
+		DrawViewImage drawView = Mockito.mock(DrawViewImage.class);
+		ContextData context = Mockito.mock(ContextData.class);
+		Mockito.when(drawView.getContext()).thenReturn(context);
+		Paint paint = Mockito.mock(Paint.class);
+		Canvas canvas = Mockito.mock(Canvas.class);
+		DisplayMetrics metrics = Mockito.mock(DisplayMetrics.class);
+
+		((BottleSpinDrawable) drawable).drawShape(drawView, paint, canvas, metrics, 1, 2);
+	}
+}
