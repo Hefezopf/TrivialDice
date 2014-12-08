32a5796a13b2bcde0bde402e460ca80686f1b12b
Markus Hopf <markus@hopf-it.de>
2014-12-08 16:33:33 +0100

    Update DrawViewDiceTest.java

--- common-trivialdice/src/test/java/de/hopf/android/common/DrawViewDiceTest.java
+++ common-trivialdice/src/test/java/de/hopf/android/common/DrawViewDiceTest.java
@@ -1,26 +1,52 @@
 package de.hopf.android.common;
 
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.robolectric.RobolectricTestRunner;
 
-import de.hopf.android.common.drawable.Drawable;
+import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.WindowManager;
+import de.hopf.android.common.drawable.base.BaseDiceDrawable;
 
+@RunWith(RobolectricTestRunner.class)
 public class DrawViewDiceTest {
 
     private DrawViewDice view;
 
     @Before
     public void setup(){
-        Drawable drawable = Mockito.mock(Drawable.class);
-        view = new DrawViewDice(null, null, 1, 2, drawable, 8, true);
+    	ContextData context = Mockito.mock(ContextData.class);
+        Resources resources = Mockito.mock(Resources.class);
+        Mockito.when(resources.getDisplayMetrics()).thenReturn(Mockito.mock(DisplayMetrics.class));
+        Mockito.when(context.getResources()).thenReturn(resources);
+        WindowManager windowManager = Mockito.mock(WindowManager.class);
+        Mockito.when(windowManager.getDefaultDisplay()).thenReturn(Mockito.mock(Display.class));
+        BaseDiceDrawable drawable = Mockito.mock(BaseDiceDrawable.class);
+        
+        view = new DrawViewDice(context, windowManager, 1, 2, drawable, 8, true);
     }
 
-    @Ignore
     @Test
     public void testOnDraw() {
-        view.onDraw(null);
-        //Assert.assertEquals(delegate.getNumber().longValue(), 1);
-    }
+    	Canvas canvas = Mockito.mock(Canvas.class);
+        view.onDraw(canvas);
+ 
+        ContextData context = Mockito.mock(ContextData.class);
+        Resources resources = Mockito.mock(Resources.class);
+        Mockito.when(resources.getDisplayMetrics()).thenReturn(Mockito.mock(DisplayMetrics.class));
+        Mockito.when(context.getResources()).thenReturn(resources);
+        WindowManager windowManager = Mockito.mock(WindowManager.class);
+        Mockito.when(windowManager.getDefaultDisplay()).thenReturn(Mockito.mock(Display.class));
+        BaseDiceDrawable drawable = Mockito.mock(BaseDiceDrawable.class);
+        
+        view = new DrawViewDice(context, windowManager, 1, 2, drawable, 8, false);
+        view.onDraw(canvas);
+    }    
+    
+    
  }
